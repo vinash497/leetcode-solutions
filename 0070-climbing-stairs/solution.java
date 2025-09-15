ff5e8f281775c1908409080b676c4983a3641882
Vinash_Atti <vinashatti2006@gmail.com>
2025-09-15 16:14:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.5 MB (99.98%)

---
+++ 0070-climbing-stairs/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+         if(n==0 || n == 1){
+            return 1;
+        }
+        int previous = 1,current = 1;
+        for(int i=2;i<=n;i++){
+            int temp = current;
+            current = previous+current;
+            previous = temp;
+        }
+        return current;
+    }
+}
