c311fe617aa2e164a7a286934eaed7d5e9326fc8
Vinash_Atti <vinashatti2006@gmail.com>
2025-09-17 15:35:22 +0000

    Sync LeetCode submission Runtime - 1 ms (99.94%), Memory - 46.5 MB (97.57%)

---
+++ 0136-single-number/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            index=index^nums[i];
+        }
+        return index;
+    }
+}
