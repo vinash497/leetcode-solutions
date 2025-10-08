49ad3d14d3df89e67ce8a70d54f2e9dc6f7bbd64
Vinash_Atti <vinashatti2006@gmail.com>
2025-10-08 14:55:02 +0000

    Sync LeetCode submission Runtime - 30 ms (99.04%), Memory - 65.2 MB (100.00%)

---
+++ 2392-successful-pairs-of-spells-and-potions/solution.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length;
+        int m = potions.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            long s = spells[i];
+            long need = (success + s - 1) / s;
+            int pos = lowerBound(potions, need);
+            ans[i] = m - pos;
+        }
+        return ans;
+    }
+
+    private int lowerBound(int[] arr, long target) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            if ((long)arr[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
