6fc4996d3a81e61a0bd3c1d7efd40da4e5db9865
Vinash_Atti <vinashatti2006@gmail.com>
2025-10-23 13:59:47 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 42 MB (100.00%)

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+      int[] arr = new int[s.length()];
+    for (int i = 0; i < arr.length; i++)
+      arr[i] = s.charAt(i) - '0';
+    for (int length = arr.length; length > 2; length--) {
+      for (int i = 0; i < length - 1; i++) {
+        arr[i] = (arr[i] + arr[i + 1]) % 10;
+      }
+    }
+
+    return arr[0] == arr[1];
+      
+    }
+}
+
