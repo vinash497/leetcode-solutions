6c0f20eedba85a24749d5d05c5f0b8d09ba2bfea
Vinash_Atti <vinashatti2006@gmail.com>
2025-11-17 15:36:37 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 65.7 MB (30.64%)

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int lastOccured = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (lastOccured != -1) {
+                    int gap = i - lastOccured - 1;
+                    if (gap < k) return false;
+                }
+                lastOccured = i;
+            }
+        }
+        return true;
+    }
+}
