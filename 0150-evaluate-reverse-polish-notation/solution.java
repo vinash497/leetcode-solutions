786e8d8974c59c98bd80104507587a13cf4843e7
Vinash_Atti <vinashatti2006@gmail.com>
2025-12-01 17:09:05 +0000

    Sync LeetCode submission Runtime - 5 ms (96.96%), Memory - 45.3 MB (63.02%)

---
+++ 0150-evaluate-reverse-polish-notation/solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a + b);
+                    break;
+                case "-":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a - b);
+                    break;
+                case "*":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a * b);
+                    break;
+                case "/":
+                    b = stack.pop();
+                    a = stack.pop();
+                    stack.push(a / b);
+                    break;
+                default:
+                    // If the token is a number, push it to the stack
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
