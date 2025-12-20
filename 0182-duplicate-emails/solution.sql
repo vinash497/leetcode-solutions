edf07e8382b7f15a9e54849e845bed5616c230f4
Vinash_Atti <vinashatti2006@gmail.com>
2025-12-20 03:47:48 +0000

    Sync LeetCode submission Runtime - 423 ms (83.20%), Memory - 0.0B (100.00%)

--- 0182-duplicate-emails/solution.sql
+++ 0182-duplicate-emails/solution.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
-SELECT email
-FROM Person
+/* Write your PL/SQL query statement below */
+SELECT email FROM Person
 GROUP BY email
 HAVING COUNT(email) > 1;
