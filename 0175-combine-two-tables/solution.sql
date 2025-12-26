ea457ab5981416655a3379173d4580660e5f50c0
Vinash_Atti <vinashatti2006@gmail.com>
2025-12-26 06:47:58 +0000

    Sync LeetCode submission Runtime - 452 ms (93.90%), Memory - 0.0B (100.00%)

--- 0175-combine-two-tables/solution.sql
+++ 0175-combine-two-tables/solution.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-select Person.firstName, Person.lastName, Address.city ,Address.state 
-FROM Person
-LEFT JOIN Address
-ON Person.personId = Address.personId;
-
+/* Write your PL/SQL query statement below */
+SELECT p.firstName,p.lastName,a.city,a.state FROM
+Person p LEFT JOIN Address a
+ON p.personId = a.personId;
