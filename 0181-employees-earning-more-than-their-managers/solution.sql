f7ccf2caf818cd2adee33b354131b011f1d6946d
Vinash_Atti <vinashatti2006@gmail.com>
2026-01-02 04:50:26 +0000

    Sync LeetCode submission Runtime - 394 ms (99.92%), Memory - 0.0B (100.00%)

--- 0181-employees-earning-more-than-their-managers/solution.sql
+++ 0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-SELECT e2.name as Employee
-FROM employee e1
-INNER JOIN employee e2 ON e1.id = e2.managerID
-WHERE
-e1.salary < e2.salary
+/* Write your PL/SQL query statement below */
+SELECT e2.name AS Employee FROM
+Employee e1 JOIN Employee e2
+ON e1.id = e2.managerId
+WHERE e1.salary < e2.salary; 
