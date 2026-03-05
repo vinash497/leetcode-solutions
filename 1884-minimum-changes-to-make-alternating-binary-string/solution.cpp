c451f20d2052c5435291a959e020afc6b5779094
Vinash_Atti <vinashatti2006@gmail.com>
2026-03-05 16:17:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (49.39%)

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int count1 = 0,count2 = 0;
+
+        for(int i=0;i<s.size();i++){
+            char expected1 = (i%2) == 0 ? '0':'1';
+            char expected2 = (i%2) == 0 ? '1':'0';
+
+            if(s[i] != expected1) count1++;
+            if(s[i] != expected2) count2++; 
+        }
+        return min(count1,count2);
+    }
+};
