fb7d22b778b3b0e73393f5262185f140bdc647c1
Vinash_Atti <vinashatti2006@gmail.com>
2026-07-26 03:04:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (79.62%)

---
+++ 4353-largest-integer-with-given-digit-sum/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestInteger(int n, int s) {
+        if(s > 9 * n) return -1;
+        int ans = 0;
+        for(int i = 0;i <n;i++){
+            int digit = min(9,s);
+            ans = ans * 10 + digit;
+            s -= digit;
+        }
+        return ans;
+    }
+};
