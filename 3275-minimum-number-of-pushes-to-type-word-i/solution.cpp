e88c35e563b6b2991cc81a93e8bf50ebcceff83b
Vinash_Atti <vinashatti2006@gmail.com>
2026-07-30 09:26:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (20.91%)

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size(),ans = 0;
+        for(int i = 0;i < n;i++) ans += (i / 8) + 1;
+        return ans;
+    }
+};
