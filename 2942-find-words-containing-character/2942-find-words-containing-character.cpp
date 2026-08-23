3cb05c8ae7e7c6c60922f018d5fa21719d8fcefd
Vinash_Atti <vinashatti2006@gmail.com>
2026-08-23 08:45:20 +0530

    Time: 4 ms (14.82%), Space: 33.1 MB (58.02%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    std::vector<int> findWordsContaining(std::vector<std::string>& words, char x) {
+        std::vector<int> result;
+        for (int i = 0; i < words.size(); ++i) {
+            if (words[i].find(x) != std::string::npos) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
